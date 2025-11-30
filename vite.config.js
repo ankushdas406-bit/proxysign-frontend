d518464167c930487c2e37d7ff581367fe09e4b8
ankush das <ankushdas406@gmail.com>
2025-11-30 21:17:56 +0530

    fix: vite base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// ✅ DEFAULT WORKING VITE CONFIG (NO HTTPS)
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,      // allow phone access
-    port: 5173       // keep same port
-  }
-})
+  base: "/", // IMPORTANT for Vercel
+});
