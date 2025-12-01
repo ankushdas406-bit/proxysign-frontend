a4ca1372847dc98ded0a2d18e30c1b563a8b2d10
ankush das <ankushdas406@gmail.com>
2025-12-01 17:50:51 +0530

    fix router: add /admin-login route

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AdminLogin from "./pages/AdminLogin.jsx";    
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import AdminLogin from "./pages/AdminLogin.jsx";
 import AdminDashboard from "./pages/AdminDashboard.jsx";
 import Teachers from "./pages/Teachers.jsx";
 import Lectures from "./pages/Lectures.jsx";
 import Attendance from "./pages/Attendance.jsx";
-import StudentScanner from './pages/StudentScanner.jsx';
-import Scanner from "./pages/Scanner.jsx";
 import Attend from "./pages/Attend.jsx";
 
-
-function App() {
+export default function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
+
+        {/* LOGIN ROUTE */}
+        <Route path="/admin-login" element={<AdminLogin />} />
+
+        {/* DASHBOARD ROUTES */}
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
         <Route path="/teachers" element={<Teachers />} />
         <Route path="/lectures" element={<Lectures />} />
         <Route path="/attendance" element={<Attendance />} />
-        <Route path="/scan" element={<StudentScanner />} />
-        <Route path="/scanner" element={<Scanner />} />
+
+        {/* STUDENT ATTEND PAGE */}
         <Route path="/attend" element={<Attend />} />
 
-        
-        {/* Admin Login Route */}
+        {/* DEFAULT ROUTE = LOGIN */}
         <Route path="/" element={<AdminLogin />} />
 
-        {/* Admin Dashboard */}
-        <Route path="/admin-dashboard" element={<AdminDashboard />} />
-
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
-
-export default App;
